21b734b6bc24a8abc7b41d994ed087a611cf06cb
Seyda1 <seydaybar@gmail.com>
2020-09-14 20:33:06 +0300

    c#

---
+++ app 6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,x;
+            bool b = true;
+            Console.WriteLine("bir başlangıç sayısı giriniz...:");
+            x = Convert.ToInt32(Console.ReadLine());
+          
+            while (b){
+                Console.WriteLine("bir sayı giriniz...:");
+                 a = Convert.ToInt32(Console.ReadLine());
+                 if (x == a) {
+                    Console.WriteLine("program durdu");
+                        break;
+                          }
+                    if (a - x > 0){
+                        Console.WriteLine("sayı ARTIYOR");
+                         }
+                     else if (a - x < 0){
+                    
+                        Console.WriteLine("sayı Azalıyor");
+                         }
+                    x = a; 
+                             
+                }
+            
+            Console.ReadLine();
+        }
+    }
+}     
+                    
+               
+                   
+                    
+   
